5ff8e1a75b61066b23b75629c4252b0ef47c13db
Luis <lp_santos29@outlook.com>
2021-12-17 06:21:25 +0100

    'cd' Done

--- src/builtins/cd_input.c
+++ src/builtins/cd_input.c
@@ -6,13 +6,12 @@
 /*   By: lpaulo-d <lpaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 22:50:10 by lpaulo-d          #+#    #+#             */
-/*   Updated: 2021/12/17 06:21:01 by lpaulo-d         ###   ########.fr       */
+/*   Updated: 2021/12/17 06:21:24 by lpaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* need deal with ~ - .. ../../ */
 void	cmd_cd(t_struct *mode)
 {
 	int	size_split_input;
